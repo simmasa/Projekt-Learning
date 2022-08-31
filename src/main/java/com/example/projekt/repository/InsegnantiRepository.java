afd42934e19ffb0fd7826f66860bdeb28fe67aed
simmasa <s.masa@hotmail.it>
2022-08-31 12:31:59 +0200

    repo create

---
+++ src/main/java/com/example/projekt/repository/InsegnantiRepository.java
@@ -0,0 +1,7 @@
+package com.example.projekt.repository;
+
+import com.example.projekt.model.Insegnanti;
+import org.springframework.data.repository.CrudRepository;
+
+public interface InsegnantiRepository extends CrudRepository<Insegnanti, Integer> {
+}
