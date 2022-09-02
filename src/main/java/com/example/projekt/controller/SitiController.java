6089ad7dc1ac302536507c7c8b0704f42672bbca
simmasa <s.masa@hotmail.it>
2022-09-02 11:52:48 +0200

    Aggiunto controller dettagli corsi

--- src/main/java/com/example/projekt/controller/SitiController.java
+++ src/main/java/com/example/projekt/controller/SitiController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,14 +24,6 @@ public class SitiController {
     @Autowired
     public CategorieRepository catRepo;
 
-//    @GetMapping
-//    public String corsi(Model m) {
-//        m.addAttribute("corsi", corsiRepo.findAll());
-//        m.addAttribute("categorie",catRepo.findAll());
-//        m.addAttribute("corsiTop",topCorsi());
-//        return "Corsi";
-//    }
-
     @GetMapping
     public String corsiQuery(@RequestParam(name = "cat",required = false) String cat, Model m) {
         if (cat!=null) {
@@ -40,14 +33,19 @@ public class SitiController {
             m.addAttribute("corsi", corsiRepo.findAll());
         }
         m.addAttribute("categorie",catRepo.findAll());
-        m.addAttribute("corsiTop",topCorsi());
+        m.addAttribute("corsiTop",topCorsi(3));
         return "Corsi";
     }
 
-    public List<Corsi> topCorsi() {
+    @GetMapping("/{id}")
+    public String corsiDetail (@PathVariable("id") Integer corsoId, Model m) {
+    return "corsiDet";
+    }
+
+    public List<Corsi> topCorsi(int numCorsi) {
         List<Corsi> completa = corsiRepo.findByOrderByNumVisualDesc();
         List<Corsi> top = new ArrayList<Corsi>();
-        for (int i=0;i<3;i++) {
+        for (int i=0;i<numCorsi;i++) {
             top.add(completa.get(i));
         }
         return top;
